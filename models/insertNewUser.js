4c4720e68174e603caa36b260168760474f512de
42ghan <ghan@student.42seoul.kr>
2022-01-07 20:47:05 +0900

    :recycle: refactor : query 비동기 처리 위해 mysql 모듈을 mysql2로 변경 #32 - mysql -> mysql2 - promise wrapper 적용해서 query callback 간소화

--- models/insertNewUser.js
+++ models/insertNewUser.js
@@ -1,30 +1,19 @@
 import consoleLogger from '../controllers/consoleLogger.js';
 
 export default async function insertNewUser(pool, user) {
-  try {
-    pool.getConnection(async (err, connection) => {
-      if (err) throw err; // 연결되지 않았을 때
-      const sql = 'INSERT INTO user SET ?';
-      await connection.query(
-        sql,
-        { name: user.username, profileImage: user.profileImage },
-        (err, rows, fields) => {
-          connection.release();
-          if (!err) {
-            consoleLogger.info('rows: ', rows);
-          } else if (err.code === 'ER_DUP_ENTRY') {
-            consoleLogger.info(
-              '=============================================\n' +
-                `이미 등록돼 있는 사용자입니다!\n` +
-                '============================================='
-            );
-          } else {
-            consoleLogger.error(`query error : ${err}`);
-          }
-        }
-      );
+  const sql = 'INSERT INTO user SET ?';
+  pool
+    .query(sql, { id: user.id, name: user.username, profileImage: user.profileImage })
+    .then((rows, fields) => consoleLogger.info('rows: ', rows))
+    .catch(err => {
+      if (err.code === 'ER_DUP_ENTRY') {
+        consoleLogger.info(
+          '=============================================\n' +
+            `이미 등록돼 있는 사용자입니다!\n` +
+            '============================================='
+        );
+      } else {
+        consoleLogger.error(`query error : ${err}`);
+      }
     });
-  } catch (err) {
-    consoleLogger.error(`Error in getConnection: ${err}`);
-  }
 }
