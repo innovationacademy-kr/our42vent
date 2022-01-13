4d20fda4eab4dd18ae41d99f54e8742c263e49da
42ghan <ghan@student.42seoul.kr>
2022-01-13 15:00:36 +0900

    :sparkles: feat : 월단위 캘린더 데이터 가공 draft v2 #56 #57 - 공휴일 데이터 OpenAPI 로 fetch & 공휴일 flag

---
+++ controllers/monthController.js
@@ -0,0 +1,32 @@
+import {
+  checkYearMonthRange,
+  getMonthDates,
+  getMonthRange,
+  mapDayEvent,
+} from '../lib/calendar/monthUtils.js';
+import getHolidays from '../lib/calendar/holidaysUtils.js';
+import selectMonthEvents from '../models/selectMonthEvents.js';
+
+// month 데이터 json 으로 가공 & client 에 전송
+export default async function monthController(req, res) {
+  try {
+    const yearParam = Number(req.params.year);
+    const monthParam = Number(req.params.month);
+
+    const { year, month } = checkYearMonthRange(yearParam, monthParam);
+    const { noWeeks, dates } = getMonthDates(year, month);
+    const { firstSQLDate, lastSQLDate } = getMonthRange(dates[0], dates.at(-1));
+    const monthEvents = await selectMonthEvents(firstSQLDate, lastSQLDate);
+    const holidays = await getHolidays(dates, year, month);
+    const dateEvents = mapDayEvent(dates, monthEvents, holidays);
+
+    return res.json({
+      dateEvents,
+      noWeeks,
+      year,
+      monthIndex: month,
+    });
+  } catch (err) {
+    return err;
+  }
+}
