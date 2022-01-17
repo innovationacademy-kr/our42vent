42fea30503287647e834d28d2ac3169da713345d
yongjulejule <lyjshow200@gmail.com>
2022-01-17 13:38:13 +0900

    :recycle: refactor : 에러 메시지 문법 수정

--- models/accessTokenRedis.js
+++ models/accessTokenRedis.js
@@ -4,7 +4,7 @@ import consoleLogger from '../lib/consoleLogger.js';
 export async function insertToken(id, token) {
   try {
     const ret = await redisClient.setEx(`r_${id}`, 1.2096e6, token); // 2주 뒤 만료
-    if (ret.localeCompare('OK')) throw new Error(`Fail to insert Token for ${id}`);
+    if (ret.localeCompare('OK')) throw new Error(`Failed to insert Token for ${id}`);
     consoleLogger.info(`insertToken : cache success : refreshToken has been issued for ${id}`);
   } catch (err) {
     consoleLogger.error('insertToken : cache error : ', err.stack);
@@ -24,7 +24,7 @@ export async function deleteToken(id) {
 export async function selectToken(id) {
   try {
     const ret = await redisClient.get(`r_${id}`);
-    if (ret === null) throw new Error(`Fail to select Token for ${id}`);
+    if (ret === null) throw new Error(`Failed to select Token for ${id}`);
     consoleLogger.info(`selectToken : cache success : refreshToken for ${id} is ${ret}`);
     return ret;
   } catch (err) {
