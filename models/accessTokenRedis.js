9281a08f9434b109bfc51e2c83a96636c117c05d
yongjulejule <lyjshow200@gmail.com>
2022-01-17 13:35:46 +0900

    :recycle: refactor : pr 리뷰 #74 반영 - 에러 메시지 수정

--- models/accessTokenRedis.js
+++ models/accessTokenRedis.js
@@ -5,19 +5,19 @@ export async function insertToken(id, token) {
   try {
     const ret = await redisClient.setEx(`r_${id}`, 1.2096e6, token); // 2주 뒤 만료
     if (ret.localeCompare('OK')) throw new Error(`Fail to insert Token for ${id}`);
-    consoleLogger.info(`insertToken : query success : refreshToken has issued for ${id}`);
+    consoleLogger.info(`insertToken : cache success : refreshToken has been issued for ${id}`);
   } catch (err) {
-    consoleLogger.error('insertToken : query error : ', err.stack);
+    consoleLogger.error('insertToken : cache error : ', err.stack);
   }
 }
 
 export async function deleteToken(id) {
   try {
     const ret = await redisClient.del(`r_${id}`);
-    if (ret !== 1) throw new Error(`Fail to delete Token for ${id}`);
-    consoleLogger.info(`deleteToken : query success : refreshToken has deleted for ${id}`);
+    if (ret !== 1) throw new Error(`Failed to delete Token for ${id}`);
+    consoleLogger.info(`deleteToken : cache success : refreshToken has been deleted for ${id}`);
   } catch (err) {
-    consoleLogger.error('deleteToken : query error : ', err.stack);
+    consoleLogger.error('deleteToken : cache error : ', err.stack);
   }
 }
 
@@ -25,10 +25,10 @@ export async function selectToken(id) {
   try {
     const ret = await redisClient.get(`r_${id}`);
     if (ret === null) throw new Error(`Fail to select Token for ${id}`);
-    consoleLogger.info(`selectToken : query success : refreshToken for ${id} is ${ret}`);
+    consoleLogger.info(`selectToken : cache success : refreshToken for ${id} is ${ret}`);
     return ret;
   } catch (err) {
-    consoleLogger.error('selectToken : query error : ', err.stack);
+    consoleLogger.error('selectToken : cache error : ', err.stack);
     return null;
   }
 }
