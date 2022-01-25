12f1926f1361f2614e6075f80d24997ea0140498
yongjulejule <lyjshow200@gmail.com>
2022-01-25 12:03:54 +0900

    :lipstick: style : 네비바 생성 #97 - nav바를 만들었습니다. - 카테고리 색깔을 변경하였습니다.

--- public/javascripts/nav/nav.js
+++ public/javascripts/nav/nav.js
@@ -1,4 +1,15 @@
-const lectureChecked = document.querySelector('#category-lecture:checked') !== null;
-const checkbox = document.querySelectorAll('input[type=checkbox]');
-console.log(checkbox);
-checkbox.forEach(box => {});
+const categories = ['특강', '시험', '해커톤 / 공모전', '세미나 / 컨퍼런스', '커뮤니티'];
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+checkboxes.forEach((item, index) => {
+  const checkbox = item;
+
+  checkbox.addEventListener('change', () => {
+    const selectedBox = document.querySelector(`.${checkbox.id.substring(9)}.category-item`);
+    if (checkbox.checked) {
+      selectedBox.innerHTML = `<i class="material-icons-outlined">done</i><div>${categories[index]}</div>`;
+    } else {
+      selectedBox.innerHTML = `<i></i><div>${categories[index]}</div>`;
+    }
+  });
+});
