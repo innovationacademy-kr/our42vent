5cbb3a7e5614dd800096469fade3b1c68638eb1b
42ghan <ghan@student.42seoul.kr>
2022-01-28 21:28:18 +0900

    :bug: fix : merge 후 탭 전환 오류 수정

--- public/javascripts/nav/tabSelectHandler.js
+++ public/javascripts/nav/tabSelectHandler.js
@@ -1,16 +1,9 @@
 import { generateMonth, renderInfo } from '../calendar/month/generateMonth.js';
 
 const allEventsTab = document.querySelector('.navbar-tab-all');
+const myEventsTab = document.querySelector('.navbar-tab-my');
 
-function tabListener() {
-  let activeInactive = [];
-  if (sessionStorage.getItem('isMyEvent')) {
-    sessionStorage.removeItem('isMyEvent');
-    activeInactive = [allEventsTab, myEventsTab];
-  } else {
-    sessionStorage.setItem('isMyEvent', '1');
-    activeInactive = [myEventsTab, allEventsTab];
-  }
+function drawTabSelection(activeInactive) {
   activeInactive[0].classList.replace('navbar-tab-inactive', 'navbar-tab-active');
   activeInactive[1].classList.replace('navbar-tab-active', 'navbar-tab-inactive');
   activeInactive[0]
@@ -19,11 +12,26 @@ function tabListener() {
   activeInactive[1]
     .querySelector('svg')
     .classList.replace('navbar-tab-active', 'navbar-tab-inactive');
-  renderInfo[0] = generateMonth();
 }
 
-allEventsTab.addEventListener('click', tabListener);
+allEventsTab.addEventListener('click', () => {
+  if (sessionStorage.getItem('isMyEvent')) {
+    sessionStorage.removeItem('isMyEvent');
+    drawTabSelection([allEventsTab, myEventsTab]);
+    renderInfo[0] = generateMonth();
+  }
+});
 
-const myEventsTab = document.querySelector('.navbar-tab-my');
+myEventsTab.addEventListener('click', () => {
+  if (!sessionStorage.getItem('isMyEvent')) {
+    sessionStorage.setItem('isMyEvent', '1');
+    drawTabSelection([myEventsTab, allEventsTab]);
+    renderInfo[0] = generateMonth();
+  }
+});
 
-myEventsTab.addEventListener('click', tabListener);
+const initialTab = sessionStorage.getItem('isMyEvent')
+  ? [myEventsTab, allEventsTab]
+  : [allEventsTab, myEventsTab];
+
+drawTabSelection(initialTab);
