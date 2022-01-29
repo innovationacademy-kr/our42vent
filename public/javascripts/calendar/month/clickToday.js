d7019b61a38ebfb27c24f090fda0290e68ba11a7
42ghan <ghan@student.42seoul.kr>
2022-01-30 01:13:10 +0900

    :sparkles: feat : TODAY 버튼 호버 시 오늘 날짜 표시 & 클릭 시 현재 달 뷰 표시

---
+++ public/javascripts/calendar/month/clickToday.js
@@ -0,0 +1,37 @@
+import { renderInfo, generateMonth } from './generateMonth.js';
+import { createElementAddClass } from '../../utils/domNodeUtils.js';
+
+const todayButton = document.querySelector('.header-today-button');
+const today = new Date();
+
+todayButton.addEventListener('click', () => {
+  const thisMonth = `${today.getFullYear()}${today.getMonth()}`;
+
+  if (sessionStorage.getItem('yearMonth') !== thisMonth) {
+    sessionStorage.setItem('yearMonth', thisMonth);
+    renderInfo[0] = generateMonth();
+  }
+});
+
+todayButton.addEventListener('mousemove', e => {
+  const prevSVGWrapper = document.querySelector('.today-svg');
+  if (prevSVGWrapper) prevSVGWrapper.remove();
+
+  const today = new Date();
+  const SVGWrapper = document.body.appendChild(createElementAddClass('div', ['today-svg']));
+
+  const boxWidth = 109;
+  SVGWrapper.innerHTML =
+    `<svg width="${boxWidth}" height="23" viewBox="0 0 ${boxWidth} 23" fill="none"` +
+    `xmlns="http://www.w3.org/2000/svg"><rect x="${boxWidth}" y="22.0771" width="${boxWidth}" height="22"` +
+    `rx="3" transform="rotate(-180 ${boxWidth} 22.0771)" fill="black" fill-opacity="0.7"/><text x="8"` +
+    `y="14" fill="#fff" width="${boxWidth}" height="26" font-family="sans-serif" font-size="11">` +
+    `오늘 ${today.getFullYear()}년 ${today.getMonth() + 1}월  ${today.getDate()}일</text></svg>`;
+  SVGWrapper.style.left = `${e.clientX - boxWidth + 2}`;
+  SVGWrapper.style.top = `${e.clientY + 12}`;
+});
+
+todayButton.addEventListener('mouseleave', () => {
+  const prevSVGWrapper = document.querySelector('.today-svg');
+  if (prevSVGWrapper) prevSVGWrapper.remove();
+});
