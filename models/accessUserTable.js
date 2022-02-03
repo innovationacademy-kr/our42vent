4292e2d19bec9cf1d2babbdfd005b0985905a61a
yongjulejule <lyjshow200@gmail.com>
2022-02-03 20:36:55 +0900

    :recycle: refactor : 유저 중복 체크시 ON DUPLICATE 사용

--- models/accessUserTable.js
+++ models/accessUserTable.js
@@ -1,17 +1,17 @@
 import pool from '../config/createMySQLPool.js';
 import { logger } from '../config/winston.js';
 
-export function insertUser(user) {
-  const sql = 'INSERT INTO user (id, name, profileImage) VALUES (?, ?, ?)';
+export async function insertUser(user) {
+  const sql =
+    'INSERT INTO user (id, name, profileImage) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE profileImage=?;';
 
-  pool
-    .execute(sql, [user.id, user.username, user.profileImage])
-    .then(rows => logger.info(`insertUser : query success : ${JSON.stringify(rows)}`))
-    .catch(err => {
-      if (err.code === 'ER_DUP_ENTRY')
-        logger.info(`insertUser : (${user.id}, ${user.username}) : 이미 등록돼 있는 사용자입니다!`);
-      else throw err;
-    });
+  const [rows] = await pool.execute(sql, [
+    user.id,
+    user.username,
+    user.profileImage,
+    user.profileImage,
+  ]);
+  logger.info(`insertUser : query success : ${JSON.stringify(rows)}`);
 }
 
 export async function selectUser(id) {
