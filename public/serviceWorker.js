50d0f9736ea574bb2864983f210654f2efadab95
42ghan <ghan@student.42seoul.kr>
2022-02-10 02:05:10 +0900

    :lipstick: style : 모바일 알림 배지 & 알림 클릭 시 캘린더 오픈

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -8,5 +8,11 @@ self.addEventListener('push', async e => {
   self.registration.showNotification(title, {
     body,
     icon: '/assets/favicons/our42vent.ico',
+    badge: '/assets/images/our42vent_badge.png',
   });
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
 });
