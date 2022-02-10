e264fbfa2eab9ce81bdbb58deaad75a6c6141b6f
Jisu <hellojs242@gmail.com>
2022-02-11 01:07:52 +0900

    :recycle: refactor: 42api key 분리 #178 - updateEvent에 쓰이는 것과 login에 쓰이는 것을 구분

--- db/getIntraEvents/getFtAccessToken.js
+++ db/getIntraEvents/getFtAccessToken.js
@@ -7,8 +7,8 @@ export default async function getFtAccessToken(redisClient) {
     if (!exToken) {
       const res = await axios.post('https://api.intra.42.fr/oauth/token', {
         grant_type: 'client_credentials',
-        client_id: process.env.FORTYTWO_APP_ID,
-        client_secret: process.env.FORTYTWO_APP_SECRET,
+        client_id: process.env.FORTYTWO_APP_ID_EVENT,
+        client_secret: process.env.FORTYTWO_APP_SECRET_EVENT,
       });
       const newToken = res.data.access_token;
       const result = await redisClient.setEx('ft_access_token', 7200, newToken);
